817ea1c3069912016fda7ac871d4a1223eb4df58
nobamb <jsh12345644@gmail.com>
2026-01-21 12:47:46 +0900

    class_python.py => ClassPython.py로 변경

--- python/python_class_or_funcion/class_python.py
+++
@@ -1,7 +0,0 @@
-# 클래스명 class_python
-# 생성자는 이름, 나이, 성별을 받음
-class class_python:
-    def __init__(self,name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
