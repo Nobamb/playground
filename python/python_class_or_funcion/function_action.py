c8d908c08b827e3e22bb3f555ac01bd4041c3e19
nobamb <jsh12345644@gmail.com>
2026-01-21 14:22:44 +0900

    age_plus, call_name_gender추가

---
+++ python/python_class_or_funcion/function_action.py
@@ -0,0 +1,21 @@
+# function_action
+# 내부에 클로저 생성
+
+
+# 나이를 더하는 함수
+def age_plus(age):
+    
+    
+    # 나이를 더함
+    age_plus += age
+    
+    # 더한 나이를 반환
+    return age_plus
+
+
+# 이름과 성별을 부르도록 출력("나야 홍길동, 남자")
+
+def call_name_gender(name, gender):
+    # 이름과 성별을 불러오고
+    # f"나야 name gender"의 형식으로 전달
+    return f"나야 {name} {gender}"
