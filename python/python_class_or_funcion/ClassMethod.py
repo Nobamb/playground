f0831d4e2498cc29e7f2a7efefd6fd57c6a4c6ca
nobamb <jsh12345644@gmail.com>
2026-01-21 13:05:05 +0900

    ClassMethod 기본 생성자까지 작성

---
+++ python/python_class_or_funcion/ClassMethod.py
@@ -0,0 +1,10 @@
+# 메소드가 포함된 클래스
+class ClassMethod:
+    # 생성자
+    # 이름, 나이, 성별을 받음
+    def __init__(self,name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        
+    # 메소드 생성
