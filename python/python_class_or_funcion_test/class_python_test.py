af3054f0f7aa3b6055f0721f07b34e14d8673786
nobamb <jsh12345644@gmail.com>
2026-01-21 12:49:22 +0900

    class_python 불러와서 테스트

---
+++ python/python_class_or_funcion_test/class_python_test.py
@@ -0,0 +1,31 @@
+# 데이터 가져오기
+from data import hong
+# # os, sys 가져오기
+import os
+import sys
+
+# 현재파일 절대경로 지정
+path_abs = os.path.abspath(__file__)
+# 현재 절대경로에서 부모경로까지 지정
+path_parent = os.path.dirname(path_abs)
+# 부모경로의 부모경로까지 지정
+path_grandparent = os.path.dirname(path_parent)
+
+# path_grandparent 경로 시스템에 추가(path_grandparent 경로도 찾게 하기 위함)
+sys.path.append(path_grandparent)
+
+# 부모 경로
+# ClassPython가져오기
+from python_class_or_funcion.ClassPython import ClassPython
+
+
+# hong 데이터 가져오기
+name = hong["name"]
+age = hong["age"]
+gender = hong["gender"]
+
+# class로 생성자 생성(classpython)
+person = ClassPython(name, age, gender)
+
+# 이름 출력
+print(person.name)
