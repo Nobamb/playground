fc4fce41fd12a994e4a88badfb856634bc823b9e
nobamb <jsh12345644@gmail.com>
2026-01-21 10:11:39 +0900

    test_import2.py 작성 완료

--- python/test_import2.py
+++ python/test_import2.py
@@ -5,5 +5,16 @@ import test_module.test_module2
 
 
 
-# test_import1 출력
-print(test_module.test_module1.test_module1(1))
+# test_module1 파일에 있는 test_module1 함수 가져오기
+
+import_function = test_module.test_module1.test_module1
+
+# test_module2 파일에 있는 test_module2 사전 가져오기
+
+import_dictionary = test_module.test_module2.test_module2
+
+# 가져온 함수, 사전을 토대로 실행
+result = import_function(import_dictionary["name"])
+
+# 실행
+print(result)
