dc6da4caf3b329360e91ff4c7c493c2533d7d6b8
nobamb <jsh12345644@gmail.com>
2026-01-21 10:17:44 +0900

    test_import3.py 완성

--- python/test_import3.py
+++ python/test_import3.py
@@ -2,3 +2,9 @@
 
 from test_module.test_module1 import test_module1
 from test_module.test_module3 import test_module3
+
+# test_module1에 test_module3대입
+result = test_module1(test_module3)
+
+# result 출력
+print(result)
