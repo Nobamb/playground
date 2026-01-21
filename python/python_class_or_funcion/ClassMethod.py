22ada50770489301802b22f54c469a5eceacff48
nobamb <jsh12345644@gmail.com>
2026-01-21 13:07:07 +0900

    ClassMethod 메서드 생성

--- python/python_class_or_funcion/ClassMethod.py
+++ python/python_class_or_funcion/ClassMethod.py
@@ -8,3 +8,10 @@ class ClassMethod:
         self.gender = gender
         
     # 메소드 생성
+    # 메소드1. 나이 1살 추가하여 출력
+    def age_plus(self):
+        self.age += 1
+        return self.age
+    # 메소드2. 이름을 부르도록 출력("나야 홍길동")
+    def call_name(self):
+        return f"나야 {self.name}"
